e3c025b903470e220d7134951f3852379b3deea4
Андрей Рузаев <ruzaev-1999@mail.ru>
2020-04-15 13:57:28 +0300

    Добавьте файлы проекта.

---
+++ BoardF/Coord.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoardF
+{
+    struct Coord
+    {
+        public int x;
+        public int y;
+
+        public Coord(int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public Coord (int size)
+        {
+            x = size - 1;
+            y = size - 1;
+        }
+        public bool OnBoard(int size)
+        {
+            if (x < 0 || x > size - 1) return false;
+            if (y < 0 || y > size - 1) return false;
+            return true;
+        }
+        public IEnumerable<Coord> YieldCoord(int size)
+        {
+            for (y = 0; y < size; y++)
+                for (x = 0; x < size; x++)
+                    yield return this;
+        }
+
+        public Coord Add(int sx, int sy)
+        {
+            return new Coord(x + sx, y + sy);
+        }
+    }
+}
---
+++ BoardF/Game.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoardF
+{
+    public class Game
+    {
+        int size;
+        Map map;
+        Coord space;
+        public int moves { get; private set; }
+
+
+        public Game(int size)
+        {
+            this.size = size;
+            map = new Map(size);
+        }
+
+        public void Start(int seed=0)
+        {
+            int digit = 0;
+            foreach(Coord xy in new Coord().YieldCoord(size))
+                    map.Set(xy, ++digit);
+            space = new Coord(size);
+            if (seed > 0)
+                Shuffle(seed);
+            moves = 0;
+        }
+
+
+        void Shuffle(int seed)
+        {
+            Random random = new Random(seed);
+            for (int j = 0; j < seed; j++)
+                PressAt(random.Next(size), random.Next(size));
+
+        }
+        public int PressAt(int x,int y)
+        {
+            return PressAt(new Coord(x, y));
+        }
+
+
+        int PressAt(Coord xy)
+        {
+            if(space.Equals(xy))
+                return 0;
+            if (xy.x != space.x &&
+                xy.y != space.y)
+                return 0;
+            int steps = Math.Abs(xy.x - space.x) +
+                Math.Abs(xy.y - space.y);
+            while (xy.x != space.x)
+                Shift(Math.Sign(xy.x - space.x), 0);
+            while (xy.y != space.y)
+                Shift(0, Math.Sign(xy.y-space.y));
+
+            moves += steps;
+            return steps;
+
+        }
+        void Shift(int sx, int sy)
+        {
+            Coord next = space.Add(sx, sy);
+            map.Copy(next, space);
+            space = next;
+        }
+
+        public int GetDigitAt(int x,int y)
+        {
+            return GetDigitAt(new Coord(x, y));
+        }
+
+        int GetDigitAt(Coord xy)
+        {
+            if (space.Equals(xy))
+                return 0;
+            return map.Get(xy);
+        }
+
+        public bool Solved()
+        {
+            if (!space.Equals(new Coord(size)))
+                return false;
+            int digit = 0;
+            foreach (Coord xy in new Coord().YieldCoord(size))
+                if (map.Get(xy) != ++digit)
+                    return space.Equals(xy);
+            return true;
+        }
+
+    }
+}
